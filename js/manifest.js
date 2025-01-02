bc3e50b7dc1566ef6c99e9aff0542a2d356ad982
volvos12 <fgetgfet@gmail.com>
2025-01-02 19:47:33 +0200

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "calculator trick",
+  "short_name": "CalculatorTrick",
+  "start_url": "/",
+  "display": "standalone", 
+  "background_color": "#00000",
+  "theme_color": "#00000",
+  "icons": [
+    {
+      "src": "../images/app-icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
